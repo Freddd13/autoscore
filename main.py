4b28732cfc46cd5cb373397c94aa8dd628dd55f3
Freddd13 <yzdyzd13@gmail.com>
2023-09-20 23:56:57 +0800

    refactor: using strategies to deal with docker, action and local

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import logging
 import logging.config
 
 
-IS_LOCAL_ENV = (getpass.getuser()=="Fred")
+IS_AUTHOR_ENV = (getpass.getuser()=="Fred")
 LOG_PATH = "app.log"
 LAST_SUCCESS_VAR_NAME = "LAST_SUCCESS_VIDEO_TIME"
 # log_file = 'myapp.log'
@@ -233,7 +233,7 @@ class YoutubeRSSHandler:
         self.proxy_dict = {
             'http': '127.0.0.1:51837',
             'https': '127.0.0.1:51837',
-        } if IS_LOCAL_ENV else {}
+        } if IS_AUTHOR_ENV else {}
 
 
     def notify_subers(self, num):
@@ -343,77 +343,152 @@ class GithubAPI:
         return response.status_code==204
 
 
-#TODO 
-class BaseUploader:
-    def __init__(self):
-        pass
 
-    def upload(self, file_path):
+##################### strategies
+class BaseStrategy:
+    def __init__(self):
+        self.last_success_video_time = None
+
+    def run(self):
         return NotImplementedError
 
-class ODUploader:
-    def __init__(self):
-        pass
-
-    def upload(self, file_path):
+    def load_config(self):
         return NotImplementedError
 
-class GDUploader:
-    def __init__(self):
-        pass
-
-    def upload(self, file_path):
+    def update_last_success_time(self):
         return NotImplementedError
+
+    def is_last_success_exists(self):
+        return NotImplementedError
+
+
+class GithubActionStrategy(BaseStrategy):
+    def __init__(self):
+        super().__init__()
+        self.load_config()
+        self.gh_api = GithubAPI(self._github_repo_token, self._github_owner_repo)
+
+    def load_config(self):
+        self.email = os.environ.get('MMS_email')
+        self.password = os.environ.get('MMS_password')
+        self.savefolder_path = os.environ.get('MMS_savefolder_path')
+
+        self.rss_url = os.environ.get('RSS_url')
+        self.max_days_difference = os.environ.get('RSS_max_days_difference')
+        self.max_trial_num = os.environ.get('RSS_max_trial_num')
+
+        self.sender = os.environ.get('Email_sender')
+        self.receivers = [os.environ.get('Email_receivers')] # TODO
+        self.smtp_host = os.environ.get('Email_smtp_host')
+        self.smtp_port = os.environ.get('Email_smtp_port')
+        self.mail_license = os.environ.get('Email_mail_license') 
+
+        # private
+        self._github_repo_token = os.environ.get('GITHUB_REPO_TOKEN')
+        self._github_owner_repo = os.environ.get('GITHUB_OWNER_REPO')
+
+    def update_last_success_time(self, latest_time_str):
+        if not self.last_success_video_time:
+            self.gh_api.create_variables(LAST_SUCCESS_VAR_NAME, latest_time_str)
+        else:
+            self.gh_api.update_variables(LAST_SUCCESS_VAR_NAME, latest_time_str)        
+        
+    def get_last_success_time(self):
+        self.last_success_video_time = os.environ.get(LAST_SUCCESS_VAR_NAME)
+
+
+
+class DockerStrategy(BaseStrategy):
+    def __init__(self):
+        super().__init__()
+
+    def load_config(self):
+        import yaml
+        with open('.localconfig.yaml', 'r') as file:
+            yaml_data = yaml.load(file, Loader=yaml.FullLoader)
+        self.email = yaml_data['MMS']['email']
+        self.password = yaml_data['MMS']['password']
+        self.savefolder_path = yaml_data['MMS']['savefolder_path']
+
+        self.rss_url = yaml_data['RSS']['url']
+        self.max_days_difference = yaml_data['RSS']['max_days_difference']
+        self.max_trial_num = yaml_data['RSS']['max_trial_num']
+
+        self.sender = yaml_data['Email']['sender']
+        self.receivers = yaml_data['Email']['receivers']
+        self.smtp_host = yaml_data['Email']['smtp_host']
+        self.smtp_port = yaml_data['Email']['smtp_port']
+        self.mail_license = yaml_data['Email']['mail_license']
+
+    def update_last_success_time(self, latest_time_str):
+        # 将时间写入当前目前.last_success_time文件
+        with open('.last_success_time', 'w') as f:
+            f.write(latest_time_str)
+
+    def get_last_success_time(self):
+        # 从.last_success_time文件中读取时间
+        if os.path.exists('.last_success_time'):
+            with open('.last_success_time', 'r') as f:
+                self.last_success_video_time =  f.read()
+
+
+
+class LocalStrategy(BaseStrategy):
+    def __init__(self):
+        super().__init__()
+
+    def load_config(self):
+        import yaml
+        with open('.localconfig.yaml', 'r') as file:
+            yaml_data = yaml.load(file, Loader=yaml.FullLoader)
+        self.email = yaml_data['MMS']['email']
+        self.password = yaml_data['MMS']['password']
+        self.savefolder_path = yaml_data['MMS']['savefolder_path']
+
+        self.rss_url = yaml_data['RSS']['url']
+        self.max_days_difference = yaml_data['RSS']['max_days_difference']
+        self.max_trial_num = yaml_data['RSS']['max_trial_num']
+
+        self.sender = yaml_data['Email']['sender']
+        self.receivers = yaml_data['Email']['receivers']
+        self.smtp_host = yaml_data['Email']['smtp_host']
+        self.smtp_port = yaml_data['Email']['smtp_port']
+        self.mail_license = yaml_data['Email']['mail_license']
+
+
+    def update_last_success_time(self, latest_time_str):
+        # 将时间写入当前目前.last_success_time文件
+        with open('.last_success_time', 'w') as f:
+            f.write(latest_time_str)
+
+
+    def get_last_success_time(self):
+        # 从.last_success_time文件中读取时间
+        if os.path.exists('.last_success_time'):
+            with open('.last_success_time', 'r') as f:
+                self.last_success_video_time =  f.read() 
         
 
 if __name__ == "__main__":
     # 0. get config
-    if IS_LOCAL_ENV:
-        import yaml
-        with open('.localconfig.yaml', 'r') as file:
-            yaml_data = yaml.load(file, Loader=yaml.FullLoader)
-        email = yaml_data['MMS']['email']
-        password = yaml_data['MMS']['password']
-        savefolder_path = yaml_data['MMS']['savefolder_path']
+    env = os.environ.get('AUTO_HALCYON_ENV')
+    assert env
 
-        rss_url = yaml_data['RSS']['url']
-        max_days_difference = yaml_data['RSS']['max_days_difference']
-        max_trial_num = yaml_data['RSS']['max_trial_num']
-
-        sender = yaml_data['Email']['sender']
-        receivers = yaml_data['Email']['receivers']
-        smtp_host = yaml_data['Email']['smtp_host']
-        smtp_port = yaml_data['Email']['smtp_port']
-        mail_license = yaml_data['Email']['mail_license']
-
+    if env == "LOCAL":
+        strategy = LocalStrategy()
+    elif env == "DOCKER":
+        strategy = DockerStrategy()
+    elif env == "GITHUB_ACTION":
+        strategy = GithubActionStrategy()
     else:
-        email = os.environ.get('MMS_email')
-        password = os.environ.get('MMS_password')
-        savefolder_path = os.environ.get('MMS_savefolder_path')
-
-        rss_url = os.environ.get('RSS_url')
-        max_days_difference = os.environ.get('RSS_max_days_difference')
-        max_trial_num = os.environ.get('RSS_max_trial_num')
-
-        sender = os.environ.get('Email_sender')
-        receivers = [os.environ.get('Email_receivers')] # TODO
-        smtp_host = os.environ.get('Email_smtp_host')
-        smtp_port = os.environ.get('Email_smtp_port')
-        mail_license = os.environ.get('Email_mail_license') 
-
-        github_repo_token = os.environ.get('GITHUB_REPO_TOKEN')
-        github_owner_repo = os.environ.get('GITHUB_OWNER_REPO')
-        last_success_video_time = os.environ.get(LAST_SUCCESS_VAR_NAME)
-
-        # logger.debug(last_success_video_time)
+        logger.error(f"env error, not support env: {env}")
+        os._exit(-1)
 
 
     ################################  Run  ################################
-    ## 1. start GithubAPI
-    gh_api = GithubAPI(github_repo_token,github_owner_repo)
-    last_time_inited = True
+    ## 1. get last success time
+    last_success_video_time = strategy.get_last_success_time()
     if not last_success_video_time:
-        last_time_inited = False
         last_success_time = (datetime.now() - timedelta(days=365)).timestamp()
         logger.debug("no last")
     else:
@@ -423,16 +498,16 @@ if __name__ == "__main__":
 
 
     ## 2. start mms handler
-    mms = MMS(email, password, savefolder_path)
+    mms = MMS(strategy.email, strategy.password, strategy.savefolder_path)
 
 
     ## 3. start RSS handler
-    yt_rss = YoutubeRSSHandler(last_success_time=last_success_time, subers=[mms], url = rss_url)
+    yt_rss = YoutubeRSSHandler(last_success_time=last_success_time, subers=[mms], url = strategy.rss_url)
     yt_rss.get_sheet_number()
 
 
     ## 4. start email handler
-    email_handler = EmailHandler(sender, smtp_host, smtp_port,mail_license,receivers)
+    email_handler = EmailHandler(strategy.sender, strategy.smtp_host, strategy.smtp_port, strategy.mail_license, strategy.receivers)
 
 
     ## 5. check result and prepare mail data
@@ -440,10 +515,8 @@ if __name__ == "__main__":
         if len(mms.file_paths) > 0: # There are new sheets
             ## update last time
             try:
-                if not last_time_inited:
-                    gh_api.create_variables(LAST_SUCCESS_VAR_NAME, yt_rss.latest_time_str)
-                else:
-                    gh_api.update_variables(LAST_SUCCESS_VAR_NAME, yt_rss.latest_time_str)
+                strategy.update_last_success_time(str(yt_rss.latest_time))
+
                 subject = "Successfully downloading Halcyon sheets"
                 content = "Success!"
                 logger.info("All sheets downloaded successfully")
---
+++ test/test_local_update_time.py
@@ -0,0 +1,33 @@
+'''
+Date: 2023-09-20 23:46:03
+LastEditors: yxt
+LastEditTime: 2023-09-20 23:55:50
+Description: 
+'''
+import os
+import time
+from datetime import datetime, timedelta
+
+
+
+def update_last_success_time(latest_time_str):
+    # 将时间写入当前目前.last_success_time文件
+    with open('.last_success_time', 'w') as f:
+        f.write(latest_time_str)
+
+
+def get_last_success_time():
+    # 从.last_success_time文件中读取时间
+    if os.path.exists('.last_success_time'):
+        with open('.last_success_time', 'r') as f:
+            last_success_video_time =  f.readline()
+            print(float(last_success_video_time))
+
+if __name__ == '__main__':
+    last_success_video_time = (datetime.now() - timedelta(days=365)).timestamp()
+
+    get_last_success_time()
+    update_last_success_time(str(
+            int(last_success_video_time)
+        )   
+    )
