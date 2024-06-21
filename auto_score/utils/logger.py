fa1e5ed888c861115a40e9954737fb9b44872b19
Hatori233 <50367552+Freddd13@users.noreply.github.com>
2024-06-21 09:41:24 +0800

    chore: update logger

--- auto_score/utils/logger.py
+++ auto_score/utils/logger.py
@@ -1,20 +1,10 @@
-'''
-Date: 2023-10-23 18:06:56
-LastEditors: Kumo
-LastEditTime: 2023-10-23 18:06:56
-Description: 
-'''
-'''
-Date: 2023-10-05 12:42:59
-LastEditors: Kumo
-LastEditTime: 2023-10-05 17:24:55
-Description: 
-'''
-import logging.config
+import logging
 import os
+import colorlog
 
 class LoggerManager:
     log_dir_prefix = 'log'
+    
     def __init__(self, log_file):
         # 确保日志目录存在
         if not os.path.exists(self.log_dir_prefix):
@@ -31,17 +21,28 @@ class LoggerManager:
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.DEBUG)
         
-        # 创建一个formatter
-        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        # 创建一个formatter（无时间和路径）
+        file_formatter = logging.Formatter('[%(levelname)s] %(name)s: %(message)s')
+        
+        # 创建一个彩色formatter
+        console_formatter = colorlog.ColoredFormatter(
+            '%(log_color)s[%(levelname)s] %(message)s',
+            log_colors={
+                'DEBUG': 'blue',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'bold_red',
+            }
+        )
         
         # 为handler设置formatter
-        file_handler.setFormatter(formatter)
-        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(file_formatter)
+        console_handler.setFormatter(console_formatter)
         
         # 为logger添加handler
         self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
-
 
     def log_method(self, func):
         def wrapper(*args, **kwargs):
@@ -51,14 +52,12 @@ class LoggerManager:
             return func(*args, **kwargs)
         return wrapper
 
-
     def apply_log_method_to_all_methods(self, cls):
         for attr_name, attr_value in cls.__dict__.items():
             if callable(attr_value):
                 setattr(cls, attr_name, self.log_method(attr_value))
         return cls
     
-
     @staticmethod
     def get_all_log_filenames():
         return [os.path.join(LoggerManager.log_dir_prefix, filename) for filename in os.listdir(LoggerManager.log_dir_prefix) if filename.endswith('.log')]
